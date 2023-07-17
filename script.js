1267ba06854e4b2d0ff19b87b44876d1640ae75f
Daniel Bindig <d.bindig@outlook.com>
2023-07-17 19:02:06 +0930

    cleaning the code up

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function() {
+
   var saveBtnEl = $('.saveBtn')
   var currentHour = dayjs().format('H')
   var currentDate = $('#currentDate')
@@ -22,16 +21,13 @@ $(document).ready(function() {
       $(this).toggleClass('future', blockTime > currentHour);
     });
   }
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+
     $('.time-block').each(function() {
       var key = $(this).attr('id');
       var value = localStorage.getItem(key);
       $(this).children('.description').val(value);
     });
 
-    // TODO: Add code to display the current date in the header of the page.
     var currentDate = dayjs().format('[The current date is: ]DD/MM/YYYY')
       $("#currentDay").text(currentDate);
 
